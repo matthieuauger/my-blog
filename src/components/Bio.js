a8ef8d8e8a715215df1bdba2c364723cfa2018b4
Matthieu Auger <matthieua@theodo.fr>
2018-10-09 23:22:59 +0200

    :microphone: Add my bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -4,7 +4,7 @@ import React from 'react'
 import 'typeface-montserrat'
 import 'typeface-merriweather'
 
-import profilePic from './profile-pic.jpg'
+import profilePic from './profile-pic.jpeg'
 import { rhythm } from '../utils/typography'
 
 class Bio extends React.Component {
@@ -18,7 +18,7 @@ class Bio extends React.Component {
       >
         <img
           src={profilePic}
-          alt={`Kyle Mathews`}
+          alt={`Matthieu Auger`}
           style={{
             marginRight: rhythm(1 / 2),
             marginBottom: 0,
@@ -27,9 +27,8 @@ class Bio extends React.Component {
           }}
         />
         <p>
-          Written by <strong>Kyle Mathews</strong> who lives and works in San
-          Francisco building useful things.{' '}
-          <a href="https://twitter.com/kylemathews">
+          Written by <strong>Matthieu Auger</strong> who lives and works in Paris building useful things.{' '}
+          <a href="https://twitter.com/matthieuauger">
             You should follow him on Twitter
           </a>
         </p>
