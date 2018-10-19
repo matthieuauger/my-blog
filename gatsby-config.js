2047f6ac8ae878a3c3a7046fa798b3663e63bedf
Matthieu Auger <matthieua@theodo.fr>
2018-10-19 13:41:00 +0200

    Add GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-127785943-1`,
       },
     },
     `gatsby-plugin-feed`,
