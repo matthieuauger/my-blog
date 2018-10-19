e6ddc3b0c67910503e19b0c5f6caed4dc34ea5ec
Matthieu Auger <matthieua@theodo.fr>
2018-10-19 18:50:08 +0200

    Show what is this blog

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -10,29 +10,34 @@ import { rhythm } from '../utils/typography'
 class Bio extends React.Component {
   render() {
     return (
-      <div
-        style={{
-          display: 'flex',
-          marginBottom: rhythm(2.5),
-        }}
-      >
-        <img
-          src={profilePic}
-          alt={`Matthieu Auger`}
-          style={{
-            marginRight: rhythm(1 / 2),
-            marginBottom: 0,
-            width: rhythm(2),
-            height: rhythm(2),
-          }}
-        />
+      <>
         <p>
-          Written by <strong>Matthieu Auger</strong> who lives and works in Paris building things.{' '}
-          <a href="https://twitter.com/matthieuauger">
-            You should follow him on Twitter
-          </a>
+          A simple blog about computing, lean, agile and everything useful enough to build quickly quality products.
         </p>
-      </div>
+        <div
+          style={{
+            display: 'flex',
+            marginBottom: rhythm(2.5),
+          }}
+        >
+          <img
+            src={profilePic}
+            alt={`Matthieu Auger`}
+            style={{
+              marginRight: rhythm(1 / 2),
+              marginBottom: 0,
+              width: rhythm(2),
+              height: rhythm(2),
+            }}
+          />
+          <p>
+            Written by <strong>Matthieu Auger</strong> who lives and works in Paris building things.{' '}
+            <a href="https://twitter.com/matthieuauger">
+              You should follow him on Twitter
+            </a>
+          </p>
+        </div>
+      </>
     )
   }
 }
