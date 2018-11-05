7fe5d7f9b40906f14c586f3ebad8e71687e193d3
Matthieu Auger <matthieua@theodo.fr>
2018-11-06 00:39:45 +0100

    :lipstick: Theme contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Formik } from 'formik';
 import get from 'lodash/get'
 import Helmet from 'react-helmet'
 
 import Layout from '../components/layout'
+import Style from '../components/ContactForm/ContactForm.style';
 
 class BlogContact extends React.Component {
   render() {
@@ -21,24 +23,93 @@ class BlogContact extends React.Component {
           title={siteTitle}
         />
         <h1>Contact</h1>
+        <Formik
+          initialValues={{ name: '', email: '', message: '' }}
+          validate={values => {
+            let errors = {};
+            if (!values.name) {
+              errors.name = 'Required';
+            }
 
-        <form name="contact" method="POST" data-netlify-honeypot="bot-field" data-netlify="true">
-          <input type="hidden" name="form-name" value="contact" />
-          <p>
-              <label>Name: <input type="text" name="name"></input></label>
-          </p>            
-          <p>
-              <label>Email: <input type="email" name="email"></input></label>
-          </p>
+            if (!values.email) {
+              errors.email = 'Required';
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            ) {
+              errors.email = 'Invalid email address';
+            }
 
-          <p>
-              <label>Review message: <textarea name="message"></textarea></label>
-          </p>
-          <p>
-              <button type="submit">Send</button>
-          </p>
-        </form>
+            if (!values.message) {
+              errors.message = 'Required';
+            }
 
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+            }, 400);
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+            /* and other goodies */
+          }) => (
+            <Style>
+              <div className="container">
+                <form name="contact" method="POST" data-netlify-honeypot="bot-field" data-netlify="true" onSubmit={handleSubmit}>
+                  <input type="hidden" name="form-name" value="contact" />
+                  <ul className="flex-outer">
+                    <li>
+                      <label>Email</label> 
+                      <input
+                        type="email"
+                        name="email"
+                        required
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.email}
+                      />
+                    </li>
+                    <li>
+                      <label>Name</label>
+                      <input
+                        type="text"
+                        name="name"
+                        required
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.name}
+                      />
+                    </li>
+                    <li>
+                      <label>Message</label>
+                      <textarea
+                        name="message"
+                        required
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.message}
+                      >{values.message}</textarea>
+                    </li>
+                    <li>
+                      <button type="submit" disabled={isSubmitting}>
+                        Send
+                      </button>
+                    </li>
+                  </ul>
+                </form>
+              </div>
+            </Style>
+          )}
+        </Formik>
       </Layout>
     )
   }
